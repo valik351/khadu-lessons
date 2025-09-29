85f74fadf3ed665c9efcd7b52c5722d3011fdb2e
Valentyn Avdieiev <av.val.100@gmail.com>
2025-09-29 10:34:43 +0200

    init

--- api/calendar-hnadu.ics.mjs
+++ api/calendar-hnadu.ics.mjs
@@ -1,4 +1,4 @@
-import {getCalendar} from "../../index.mjs";
+import {getCalendar} from "../index.mjs";
 
 export default async function handler(req, res) {
     res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
